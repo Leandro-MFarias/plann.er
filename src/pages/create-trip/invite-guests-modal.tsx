08deeaebf343f62edd4b9598104c546a1ebdb636
Leandro Farias <leandrorf1606@gmail.com>
2024-07-11 00:29:39 -0300

    component button

--- src/pages/create-trip/invite-guests-modal.tsx
+++ src/pages/create-trip/invite-guests-modal.tsx
@@ -1,5 +1,6 @@
 import { AtSign, Plus, X } from "lucide-react"
 import { FormEvent } from "react"
+import { Button } from "../../components/button"
 
 interface InviteGuestsModalProps {
     closeGuestsModal: () => void
@@ -8,13 +9,13 @@ interface InviteGuestsModalProps {
     removeEmailFromInvites: (email: string) => void
 }
 
-export function InviteGuestsModal({ 
-    addNewEmailToInvite, 
-    closeGuestsModal, 
-    emailsToInvite, 
-    removeEmailFromInvites 
+export function InviteGuestsModal({
+    addNewEmailToInvite,
+    closeGuestsModal,
+    emailsToInvite,
+    removeEmailFromInvites
 }: InviteGuestsModalProps) {
-    
+
     return (
         <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
             <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -57,10 +58,10 @@ export function InviteGuestsModal({
                         />
                     </div>
 
-                    <button type='submit' className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+                    <Button type="submit" variant="primary">
                         Convidar
                         <Plus className='size-5' />
-                    </button>
+                    </Button>
                 </form>
             </div>
         </div>
--- src/pages/trip-datails/create-activity-modal.tsx
+++ src/pages/trip-datails/create-activity-modal.tsx
@@ -1,4 +1,5 @@
-import { Calendar, Tag, X } from "lucide-react"
+import { Calendar, Settings2, Tag, X } from "lucide-react"
+import { Button } from "../../components/button"
 
 interface CreateActivityModalProps {
     closeCreateActivityModal: () => void
@@ -45,9 +46,9 @@ export function CreateActivityModal({
                         </div>
                     </div>
 
-                    <button type='submit' className='w-full bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center justify-center gap-2 hover:bg-lime-400'>
+                    <Button variant="primary" size="full">
                         Salvar atividade
-                    </button>
+                    </Button>
                 </form>
             </div>
         </div>
